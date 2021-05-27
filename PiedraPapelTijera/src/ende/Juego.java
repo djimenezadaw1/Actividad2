6cffc4c666ee702ea327ece78eef683b8f2d0f41
Daniel <djimenezadaw1@gmail.com>
2021-05-27 13:11:58 +0200

    Ultima mejora: Sacamos constantes, ponemos llaves en su sitio y cambiamos clases wrapper por primitivas

--- PiedraPapelTijera/src/ende/Juego.java
+++ PiedraPapelTijera/src/ende/Juego.java
@@ -7,6 +7,9 @@ package ende;
 
 public class Juego 
 {
+	private static final String TIJERAS = "tijeras";
+	private static final String PAPEL = "papel";
+	private static final String PIEDRA = "piedra";
     private static int VICTORIAP1 = 0;
     private static int VICTORIAP2 = 0;
 
@@ -15,8 +18,8 @@ public class Juego
         Jugador p1=new Jugador();
         Jugador p2=new Jugador();
         boolean finDeJuego=false;  
-        Integer rondasJugadas = 1;    // Número de rondas jugadas
-        Integer empates = 0;
+        int rondasJugadas = 1;    // Número de rondas jugadas
+        int empates = 0;
         String opcionJugador1, opcionJugador2;
         
         // Bucle de juego
@@ -28,34 +31,34 @@ public class Juego
             opcionJugador2 = p2.opcion_al_azar();
             System.out.println("\tJugador 2: " + opcionJugador2+"\t Jugador 2 - Partidas ganadas: " + p2.getExitos());
             
-            if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("papel"))) {
+            if((opcionJugador1.equals(PIEDRA))&&(opcionJugador2.equals(PAPEL))) {
             	VICTORIAP2++;
             	p2.setExitos(VICTORIAP2);
             	System.out.println("\n\t\t\t Jugador 2 GANA \n");
                 
                 
             }
-            else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("piedra"))) {
+            else if((opcionJugador1.equals(PAPEL))&&(opcionJugador2.equals(PIEDRA))) {
                 System.out.println("\n\t\t   Jugador 1 GANA \n");
                 VICTORIAP1++;
                 p1.setExitos(VICTORIAP1);
             }
-            else if((opcionJugador1.equals("piedra"))&&(opcionJugador2.equals("tijeras"))) {
+            else if((opcionJugador1.equals(PIEDRA))&&(opcionJugador2.equals(TIJERAS))) {
                 System.out.println("\n\t\t   Jugador 1 GANA \n");
                 VICTORIAP1++;
                 p1.setExitos(VICTORIAP1);
             }
-            else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("piedra"))) {
+            else if((opcionJugador1.equals(TIJERAS))&&(opcionJugador2.equals(PIEDRA))) {
             	VICTORIAP2++;
             	p2.setExitos(VICTORIAP2);
                 System.out.println("\n\t\t   Jugador 2 GANA \n");
             }
-            else if((opcionJugador1.equals("tijeras"))&&(opcionJugador2.equals("papel"))) {
+            else if((opcionJugador1.equals(TIJERAS))&&(opcionJugador2.equals(PAPEL))) {
             	VICTORIAP1++;
                 p1.setExitos(VICTORIAP1);
                 System.out.println("\n\t\t   Jugador 1 GANA \n");
             }
-            else if((opcionJugador1.equals("papel"))&&(opcionJugador2.equals("tijeras"))) {
+            else if((opcionJugador1.equals(PAPEL))&&(opcionJugador2.equals(TIJERAS))) {
             	VICTORIAP2++;
             	p2.setExitos(VICTORIAP2);
             	System.out.println("\n\t\t   Jugador 2 GANA \n");
@@ -77,6 +80,9 @@ public class Juego
  *
  */
 class Jugador{
+	private static final String TIJERAS = "tijeras";
+	private static final String PAPEL = "papel";
+	private static final String PIEDRA = "piedra";
 	// Atributos
 	private int exitos;      // número de partidas ganadas
    
@@ -86,16 +92,16 @@ class Jugador{
     public String opcion_al_azar()
     {
         String opcion="";
-        Integer c = (int)(Math.random() * 3);
+        int c = (int)(Math.random() * 3);
         switch(c){
             case 0:
-            	opcion=("piedra");
+            	opcion=PIEDRA;
                 break;
             case 1:
-            	opcion=("papel");
+            	opcion=PAPEL;
                 break;
             case 2:
-            	opcion=("tijeras");
+            	opcion=TIJERAS;
         }
         return opcion;
     }
